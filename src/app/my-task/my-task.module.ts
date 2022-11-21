fe9d3ed6c1dcae9c4e1912445727bc0c3c695b0c
anteneh <anteshu123@gmail.com>
2022-11-21 05:10:47 +0000

    mytask issufixed

--- src/app/my-task/my-task.module.ts
+++ src/app/my-task/my-task.module.ts
@@ -1,20 +1,24 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import {MyTaskRoutingModule} from './my-task-routing.module';
-import {TaskComponent} from './task.component';
-import {MyTaskComponent} from './my-task/my-task.component';
-import {SupervisorTaskComponent} from './supervisor-task/supervisor-task.component';
-import {MyTaskService} from './my-task.service';
-import {TableModule} from 'primeng/table';
-import {TranslateModule} from '@ngx-translate/core';
+import { MyTaskRoutingModule } from './my-task-routing.module';
+import { TaskComponent } from './task.component';
+import { MyTaskComponent } from './my-task/my-task.component';
+import { SupervisorTaskComponent } from './supervisor-task/supervisor-task.component';
+import { MyTaskService } from './my-task.service';
+import { TableModule } from 'primeng/table';
+import { TranslateModule } from '@ngx-translate/core';
+import { Angular2PromiseButtonModule } from 'angular2-promise-buttons';
+import { NgxSmartModalModule } from 'ngx-smart-modal';
 
 @NgModule({
   imports: [
+    NgxSmartModalModule,
     CommonModule,
     TableModule,
     MyTaskRoutingModule,
     TranslateModule.forChild({}),
+    Angular2PromiseButtonModule.forRoot()
   ],
   declarations: [TaskComponent, MyTaskComponent, SupervisorTaskComponent],
   providers: [MyTaskService]
--- src/app/my-task/my-task.service.ts
+++ src/app/my-task/my-task.service.ts
@@ -8,6 +8,7 @@ import { environment } from "../../environments/environment";
 export class MyTaskService {
   private MytasksUrl = environment.rootPath + "BPEL/GetlistofTodo"; // URL to web api
   private SupervisertasksUrl = environment.rootPath + "BPEL/GetlistofTodo"; // URL to web api
+  private IsLockedBy_OtherUserUrl = environment.rootPath + 'BPEL/IsLockedBy_OtherUser';
 
   constructor(private http: HttpClient) {}
   getMytasks(orgid) {
@@ -27,5 +28,8 @@ export class MyTaskService {
         environment.username +
         "&orgid=00000000-0000-0000-0000-000000000000&lanid=10D04E8B-3361-E111-95D5-00E04C05559B"
     );
+  }
+  IsLockedBy_OtherUser(todoid) {
+    return this.http.get(this.IsLockedBy_OtherUserUrl + '?todoid=' + todoid + '&Username=' + environment.username);
   }
 }
